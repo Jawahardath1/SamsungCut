6dbcc3293e5956801d6c1ae813ee8d66504b90ea
Jawahar Dath Thangirala <jawahardaththangirala@jawahars-macbook-pro.local>
2019-02-04 14:21:43 -0500

    Commit

--- SamsungCut/Controller/CheckInViewController.swift
+++ SamsungCut/Controller/CheckInViewController.swift
@@ -8,15 +8,60 @@
 
 import UIKit
 import DLRadioButton
+import iOSDropDown
 
 class CheckInViewController: UIViewController {
 
     
+    @IBOutlet weak var guestName: UITextField!
+    
+    @IBOutlet var customData: [DropDown]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let option =  Options()
+        //print("Selected GuestName: \(guestName.text)")
+        
+        print(customData.count)
+        
+        // Custom DropDown Data
+        self.customData[0].optionArray = option.countries
+        self.customData[0].optionIds = option.ids
+        self.customData[0].isSearchEnable = false
+        
+        self.customData[1].optionArray = option.countries
+        self.customData[1].optionIds = option.ids
+        self.customData[1].isSearchEnable = false
+        
+        self.customData[0].didSelect(completion: { (selected, index, id)  in
+//            self.mainDropDown.isSearchEnable = Bool(selected)!
+            print("Selected Collection1 String: \(selected) \n index: \(index) \n Id: \(id)")
+        })
+        
+        self.customData[1].didSelect(completion: { (selected, index, id) in
+            if #available(iOS 11.0, *) {
+                print("Selected Collection2 String: \(selected) \n index: \(index) \n Id: \(id)")
+            } else {
+                // Fallback on earlier versions
+            }
+        })
+        
+        //segment.addTarget(self, action: #selector(CheckInViewController.segmentedDidChange(_:)), for: .valueChanged)
+        
     }
 
+    @objc func segmentedDidChange(_ sender: UISegmentedControl) {
+        let index = sender.selectedSegmentIndex
+//        if index == 0 {
+//            customView.isHidden = true
+//            cstmbutn.isHidden = false
+//        }else{
+//            customView.isHidden = false
+//
+//        }
+        
+    }
     
     @IBAction func selectServiceButton(_ sender: DLRadioButton) {
         
@@ -28,4 +73,6 @@ class CheckInViewController: UIViewController {
             print(String(format: "%@ is selected.", sender.selected()!.titleLabel!.text!))
         }
     }
+    
+    
 }
